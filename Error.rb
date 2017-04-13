3600c3249e2f5bc7a4f4d522fa1f151f18604543
Greg <probablyanawesomegame@gmail.com>
2017-04-13 00:48:05 -0500

    Added Error Map

---
+++ Error.rb
@@ -0,0 +1,7 @@
+$game_message.add("I am Error.\n\n\n")
+$game_message.add("I am all.")
+
+#event = $game_map.events[2]
+#$game_message.add(event.inspect)
+#page = event.instance_variable_get(:@page)
+
