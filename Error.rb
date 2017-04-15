fe4c98565d6e0f9207ad21a205ebe1503b8c684d
Greg <probablyanawesomegame@gmail.com>
2017-04-15 14:12:13 -0500

    Finished up

--- Error.rb
+++ Error.rb
@@ -2,6 +2,11 @@ $game_message.add("I am Error.\n\n\n")
 $game_message.add("I am all.")
 
 #event = $game_map.events[2]
-#$game_message.add(event.inspect)
+#$game_message.add(event.inspect) # Not all will fit on screen.
+#File.open("check.txt", 'w') { |file| file.write(event.inspect) }
 #page = event.instance_variable_get(:@page)
 
+### Warp to any map, any location. Smaller numbers will probably work better.
+#$game_player.reserve_transfer(map_id, x, y, direction) # Direction should be 2 (probably, not tested).
+
+### Figure out the rest yourself.
